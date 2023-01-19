c35bfc0b63ff0d056ff5f45132c31eb4c5c7bdb2
Aditya <aditya.gajbhiye.20@gmail.com>
2023-01-19 10:33:19 +0000

    added header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { Header } from "./components/Header";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <>
+      <Header />
+      <Dashboard />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-item">Dashboard</div>
+      <div className="header-item">Form</div>
+      <div className="header-item">Chart</div>
+      <div className="header-item">Paginate</div>
+    </div>
+  );
+};
+
+export { Header };
